7ce912e726d0a92eafbe96d2e5825f3e59698e6b
asfrank <asfrank@qq.com>
2019-11-21 23:58:56 +0800

    models done

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -3,9 +3,11 @@ from datetime import datetime
 from django.db import models
 
 from apps.users.models import BaseModel
+from apps.organizations.models import Teacher
 
 
 class Course(BaseModel):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name='讲师')
     name = models.CharField(verbose_name='课程名', max_length=50)
     desc = models.CharField(verbose_name='课程描述', max_length=300)
     learn_times = models.IntegerField(default=0, verbose_name='学习时长（分钟数）')
